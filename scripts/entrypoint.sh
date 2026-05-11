43bbd87bd63fa26c1b5cc359c93a2fb50b8e9d6d
TrueOsiris <tim@chaubet.be>
2026-05-11 08:31:20 +0200

    refactor: add root password update in entrypoint script using environment variable

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,9 @@
 # /scripts/entrypoint.sh
 # Author: Tim Chaubet
 
+# Update the root password using the environment variable passed from docker-compose
+echo "root:${SSH_PASSWORD}" | chpasswd
+
 # Start the SSH service
 service ssh start || /usr/sbin/sshd
 
